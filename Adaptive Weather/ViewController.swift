8998669b0831f427e7800971b86371b118619539
Brian Bansenauer <brianb@cascadia.edu>
2018-11-09 22:16:15 -0800

    Initial Commit

---
+++ Adaptive Weather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Adaptive Weather
+//
+//  Created by Brian Bansenauer on 11/9/18.
+//  Copyright © 2018 Cascadia College. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
